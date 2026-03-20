ade5ec0ccab9f6771c7a78c2dfbe7aef1195a6de
PURVA JAIN <pj8602@srmist.edu.in>
2026-03-20 14:24:32 +0530

    mybookingstayapp

---
+++ src/BookMyStay.java
@@ -0,0 +1,10 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        String appName = "Hotel Booking Management System";
+        String version = "v1.0";
+
+        System.out.println("Welcome to " + appName);
+        System.out.println("Version: " + version);
+        System.out.println("Application started successfully.");
+    }
+}
