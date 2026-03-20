55ce40ef9d2d7577c243300a6c5064c721a088af
PURVA JAIN <pj8602@srmist.edu.in>
2026-03-20 14:39:57 +0530

    usecase2

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,10 +1,64 @@
 public class BookMyStay {
-    public static void main(String[] args) {
-        String appName = "Hotel Booking Management System";
-        String version = "v1.0";
 
-        System.out.println("Welcome to " + appName);
-        System.out.println("Version: " + version);
-        System.out.println("Application started successfully.");
+    public static void main(String[] args) {
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
+}
+
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sq.ft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 200, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 350, 1800);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 600, 3500);
     }
 }
